4f4e1c3b193ebe8a62a1d820369ff6600d3a028c
deovinsum <gh@deovinsum.ru>
2024-03-05 22:26:39 +0300

    customizing navi and toolbar

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -2,59 +2,18 @@
 
 import SwiftUI
 
-@Observable
-class PathStore {
-    var path: NavigationPath {
-        didSet {
-            save()
-        }
-    }
-    
-    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
-    
-    init() {
-        if let data = try? Data(contentsOf: savePath) {
-            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
-                path = NavigationPath(decoded)
-                return
-            }
-        }
-        
-        path = NavigationPath()
-    }
-    
-    func save() {
-        guard let representations = path.codable else { return }
-        
-        do {
-            let data = try JSONEncoder().encode(representations)
-            try data.write(to: savePath)
-        } catch {
-            print("Failed to save data path")
-        }
-    }
-}
-
-
-struct DetailView: View {
-    var number: Int
-    
-    var body: some View {
-        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
-            }
-    }
-
 struct ContentView: View {
     
-    @State private var pathStore = PathStore()
-    
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i)
-                }
+        NavigationStack {
+            List(0..<100) { i in
+                Text("\(i)")
+            }
+            .navigationTitle("Hello world!")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue)
+            .toolbarColorScheme(.dark)
+            .toolbar(.hidden, for: .navigationBar)
         }
     }
 }
