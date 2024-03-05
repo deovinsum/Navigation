ebe7cf08bf6c320e0092e17d4a75ca5d532cc6b3
deovinsum <gh@deovinsum.ru>
2024-03-05 22:16:12 +0300

    observable class for path navi

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -2,34 +2,58 @@
 
 import SwiftUI
 
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representations = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representations)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save data path")
+        }
+    }
+}
+
 
 struct DetailView: View {
     var number: Int
-//    @Binding var path: [Int]
-    @Binding var path: NavigationPath
     
     var body: some View {
         NavigationLink("Go to random number", value: Int.random(in: 1...1000))
             .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("DeleteAll") {
-//                    path.removeAll()
-                    path = NavigationPath()
-                }
             }
     }
-}
 
 struct ContentView: View {
     
-//    @State private var path: [Int] = []
-    @State private var path = NavigationPath()
+    @State private var pathStore = PathStore()
     
     var body: some View {
-        NavigationStack(path: $path) {
-            DetailView(number: 0, path: $path)
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0)
                 .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $path)
+                    DetailView(number: i)
                 }
         }
     }
